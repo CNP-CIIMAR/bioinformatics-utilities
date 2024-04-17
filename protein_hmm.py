7791b9f8231e30e1f4ffb615f339b55fd158944a
CNP team <137802300+CNP-CIIMAR@users.noreply.github.com>
2024-04-17 17:19:49 +0100

    Update protein_hmm.py

--- protein_hmm.py
+++ protein_hmm.py
@@ -3,16 +3,11 @@ import subprocess
 import argparse
 import sys
 import pandas as pd
-	
-import os
-import subprocess
-import argparse
-import pandas as pd
 
 def main(models_dir, fastas_dir, output_dir):
     EVALUE = "0.00000000000000000001"
 
-    # Verificar e criar o diretÃ³rio de saÃ­da, se necessÃ¡rio
+    # Verificar e criar o diretÃƒÂ³rio de saÃƒÂ­da, se necessÃƒÂ¡rio
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -20,19 +15,20 @@ def main(models_dir, fastas_dir, output_dir):
     fastas = [os.path.join(fastas_dir, f) for f in os.listdir(fastas_dir) if f.endswith(".faa")]
 
     # Coletar todos os modelos HMM
-    models = [os.path.join(models_dir, f) for f in os.listdir(models_dir) if f.endswith(".hmm")]
+    #models = [os.path.join(models_dir, f) for f in os.listdir(models_dir) if f.endswith(".HMM")]
+    models = [os.path.join(models_dir, f) for f in os.listdir(models_dir) if f.lower().endswith(".hmm")]
 
     # Loop em cada modelo HMM
     for model in models:
-        model_name = os.path.basename(model).replace(".hmm", "")
+        model_name = os.path.basename(model).replace(".HMM", "")
         all_results = []
 
         # Loop em cada arquivo FASTA
         for fasta in fastas:
-            # Extrair o nÃºmero de acesso do nome do arquivo FASTA
+            # Extrair o nÃƒÂºmero de acesso do nome do arquivo FASTA
             accession = os.path.basename(fasta).split(".")[0]
 
-            # Definir o nome do arquivo de saÃ­da com base no modelo e no arquivo FASTA
+            # Definir o nome do arquivo de saÃƒÂ­da com base no modelo e no arquivo FASTA
             output_file = os.path.join(output_dir, f"{model_name}_{accession}_tblout.txt")
 
             cmd = f"hmmsearch --domE {EVALUE} --tblout {output_file} {model} {fasta}"
@@ -45,13 +41,13 @@ def main(models_dir, fastas_dir, output_dir):
                 print(f"Erro ao executar: {cmd}")
                 print(f"Mensagem de erro: {e.output}")
 
-        # ApÃ³s o loop de fasta, mas ainda dentro do loop de modelo:
+        # ApÃƒÂ³s o loop de fasta, mas ainda dentro do loop de modelo:
         if all_results:
             final_df = pd.concat(all_results, ignore_index=True)
             final_output_file = os.path.join(output_dir, f"{model_name}_all_results.tsv")
             write_table_to_tsv(final_df, final_output_file)
 
-    print("Busca e conversÃ£o concluÃ­das!")
+    print("Busca e conversÃƒÂ£o concluÃƒÂ­das!")
 
 
 def read_hmmer(path, program="hmmsearch", format="tblout", add_header_as_index=False, verbose=True):
@@ -142,10 +138,10 @@ def read_hmmer(path, program="hmmsearch", format="tblout", add_header_as_index=F
         df.set_index(header, append=True, inplace=True)
         df.index.names = ["header", "index"]
 
-    # Extrair a espÃ©cie de cada linha da coluna "query_description" usando expressÃµes regulares
+    # Extrair a espÃƒÂ©cie de cada linha da coluna "query_description" usando expressÃƒÂµes regulares
     df['Specie'] = df[('identifier', 'query_description')].str.extract(r'\[(.*?)\]')
 
-    # Verifique se a extraÃ§Ã£o foi bem-sucedida e preencha os valores ausentes
+    # Verifique se a extraÃƒÂ§ÃƒÂ£o foi bem-sucedida e preencha os valores ausentes
     if df['Specie'].isna().any():
         print("Warning: Some entries did not have species information extracted successfully.")
         df['Specie'].fillna("Unknown", inplace=True)
@@ -170,12 +166,13 @@ def convert_hmmer_table(input_path, output_path):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Execute hmmsearch em arquivos .faa usando modelos do diretÃ³rio fornecido.")
-    parser.add_argument("models_dir", help="DiretÃ³rio contendo os modelos HMM (.hmm).")
-    parser.add_argument("fastas_dir", help="DiretÃ³rio contendo os arquivos FASTA (.faa).")
-    parser.add_argument("output_dir", help="DiretÃ³rio para salvar os resultados.")
+    parser = argparse.ArgumentParser(description="Execute hmmsearch em arquivos .faa usando modelos do diretÃƒÂ³rio fornecido.")
+    parser.add_argument("models_dir", help="DiretÃƒÂ³rio contendo os modelos HMM (.hmm).")
+    parser.add_argument("fastas_dir", help="DiretÃƒÂ³rio contendo os arquivos FASTA (.faa).")
+    parser.add_argument("output_dir", help="DiretÃƒÂ³rio para salvar os resultados.")
     args = parser.parse_args()
 
     main(args.models_dir, args.fastas_dir, args.output_dir)
+
 
 
