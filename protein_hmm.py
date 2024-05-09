60d77de8edcc279a57c0a4a54ff6289643beed84
CNP team <137802300+CNP-CIIMAR@users.noreply.github.com>
2024-05-09 17:54:31 +0100

    Update protein_hmm.py

--- protein_hmm.py
+++ protein_hmm.py
@@ -7,7 +7,7 @@ import pandas as pd
 def main(models_dir, fastas_dir, output_dir):
     EVALUE = "0.00000000000000000001"
 
-    # Verificar e criar o diretÃƒÂ³rio de saÃƒÂ­da, se necessÃƒÂ¡rio
+    # Verificar e criar o diretório de saída, se necessário
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -15,7 +15,6 @@ def main(models_dir, fastas_dir, output_dir):
     fastas = [os.path.join(fastas_dir, f) for f in os.listdir(fastas_dir) if f.endswith(".faa")]
 
     # Coletar todos os modelos HMM
-    #models = [os.path.join(models_dir, f) for f in os.listdir(models_dir) if f.endswith(".HMM")]
     models = [os.path.join(models_dir, f) for f in os.listdir(models_dir) if f.lower().endswith(".hmm")]
 
     # Loop em cada modelo HMM
@@ -25,36 +24,50 @@ def main(models_dir, fastas_dir, output_dir):
 
         # Loop em cada arquivo FASTA
         for fasta in fastas:
-            # Extrair o nÃƒÂºmero de acesso do nome do arquivo FASTA
-            accession = os.path.basename(fasta).split(".")[0]
+            # Extrair o número de acesso do genoma do nome do arquivo FASTA
+            genome_accession = os.path.basename(fasta).split("_", 2)[:2]
+            genome_accession = "_".join(genome_accession)
 
-            # Definir o nome do arquivo de saÃƒÂ­da com base no modelo e no arquivo FASTA
-            output_file = os.path.join(output_dir, f"{model_name}_{accession}_tblout.txt")
+            # Definir o nome do arquivo de saída com base no modelo e no arquivo FASTA
+            output_file = os.path.join(output_dir, f"{model_name}_{genome_accession}_tblout.txt")
+
+            # Incluir o número de acesso do genoma como um comentário no início do arquivo
+            with open(output_file, "w") as out_f:
+                out_f.write(f"# Genome_accession: {genome_accession}\n")
 
             cmd = f"hmmsearch --domE {EVALUE} --tblout {output_file} {model} {fasta}"
 
             try:
                 subprocess.check_call(cmd, shell=True)
                 df = read_hmmer(output_file)
+                
+                # Adicionar o número de acesso do genoma como uma coluna na tabela
+                df['Genome_accession'] = genome_accession
+                
                 all_results.append(df)
             except subprocess.CalledProcessError as e:
                 print(f"Erro ao executar: {cmd}")
                 print(f"Mensagem de erro: {e.output}")
 
-        # ApÃƒÂ³s o loop de fasta, mas ainda dentro do loop de modelo:
+        # Após o loop de fasta, mas ainda dentro do loop de modelo:
         if all_results:
+            # Concatenar todos os resultados em um único DataFrame
             final_df = pd.concat(all_results, ignore_index=True)
+            
+            # Definir o nome do arquivo de saída para a tabela final concatenada
             final_output_file = os.path.join(output_dir, f"{model_name}_all_results.tsv")
+            
+            # Escrever a tabela final concatenada com o número de acesso do genoma
             write_table_to_tsv(final_df, final_output_file)
 
-    print("Busca e conversÃƒÂ£o concluÃƒÂ­das!")
+    print("Busca e conversão concluídas!")
 
 
 def read_hmmer(path, program="hmmsearch", format="tblout", add_header_as_index=False, verbose=True):
     assert_acceptable_arguments(program, {"hmmsearch", "hmmscan"})
     assert_acceptable_arguments(format, {"tblout", "domtblout"})
 
-   # Criar um dataframe vazio para 'tblout'
+    # Criar um dataframe vazio para 'tblout'
     empty_df = pd.DataFrame(columns=pd.MultiIndex.from_tuples(
         [
             ("identifier", "target_name"),
@@ -138,10 +151,10 @@ def read_hmmer(path, program="hmmsearch", format="tblout", add_header_as_index=F
         df.set_index(header, append=True, inplace=True)
         df.index.names = ["header", "index"]
 
-    # Extrair a espÃƒÂ©cie de cada linha da coluna "query_description" usando expressÃƒÂµes regulares
+    # Extrair a espécie de cada linha da coluna "query_description" usando expressões regulares
     df['Specie'] = df[('identifier', 'query_description')].str.extract(r'\[(.*?)\]')
 
-    # Verifique se a extraÃƒÂ§ÃƒÂ£o foi bem-sucedida e preencha os valores ausentes
+    # Verifique se a extração foi bem-sucedida e preencha os valores ausentes
     if df['Specie'].isna().any():
         print("Warning: Some entries did not have species information extracted successfully.")
         df['Specie'].fillna("Unknown", inplace=True)
@@ -160,19 +173,15 @@ def write_table_to_tsv(df, output_path):
     df.to_csv(output_path, sep="\t", index=False)
 
 
-def convert_hmmer_table(input_path, output_path):
-    df = read_hmmer(input_path)
-    write_table_to_tsv(df, output_path)
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Execute hmmsearch em arquivos .faa usando modelos do diretÃƒÂ³rio fornecido.")
-    parser.add_argument("models_dir", help="DiretÃƒÂ³rio contendo os modelos HMM (.hmm).")
-    parser.add_argument("fastas_dir", help="DiretÃƒÂ³rio contendo os arquivos FASTA (.faa).")
-    parser.add_argument("output_dir", help="DiretÃƒÂ³rio para salvar os resultados.")
+    parser = argparse.ArgumentParser(description="Execute hmmsearch em arquivos .faa usando modelos do diretório fornecido.")
+    parser.add_argument("models_dir", help="Diretório contendo os modelos HMM (.hmm).")
+    parser.add_argument("fastas_dir", help="Diretório contendo os arquivos FASTA (.faa).")
+    parser.add_argument("output_dir", help="Diretório para salvar os resultados.")
     args = parser.parse_args()
 
     main(args.models_dir, args.fastas_dir, args.output_dir)
+
 
 
 
